ea8c3ef70d5257036d922e56a66864f16a97865e
ameer ali vk <ameervk1875@gamil.com>
2025-12-29 19:47:10 +0530

    afterfixing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,53 @@
-import React, { useEffect, useRef } from 'react'
-import "./Navbar.css"
-import Logo from "../../assets/logo.png"
-import search_icon from "../../assets/search_icon.svg"
-import bell_icon from "../../assets/bell_icon.svg"
-import prfile_img from "../../assets/profile_img.png"
-import caret_icon from "../../assets/caret_icon.svg"
-import { Logout } from '../../firebase'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useRef } from "react";
+import "./Navbar.css";
+import Logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import prfile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
+import { Logout } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 export const Navbar = () => {
-  const navigate = useNavigate()
-  const navRef = useRef()
-  useEffect(()=>{
-    window.addEventListener("scroll",()=>{
-      if(window.scrollY >= 80){
-        navRef.current.classList.add('nav-dark')
-      }else{
-        navRef.current.classList.remove('nav-dark')
+  const navigate = useNavigate();
+  const navRef = useRef();
+  // useEffect(()=>{
+  //   window.addEventListener("scroll",()=>{
+  //     if(window.scrollY >= 80){
+  //       navRef.current.classList.add('nav-dark')
+  //     }else{
+  //       navRef.current.classList.remove('nav-dark')
+  //     }
+  //   })
+  // },[])
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!navRef.current) return; // <-- safety check
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add("nav-dark");
+      } else {
+        navRef.current.classList.remove("nav-dark");
       }
-    })
-  },[])
-  function navbarchange(){
-    navigate('/wishlist')
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function navbarchange() {
+    navigate("/wishlist");
   }
-  function homenavigate(){
-    navigate("/")
+  function homenavigate() {
+    navigate("/");
   }
-  function handleLogout(){
-    Logout()
-    navigate('/login')
+  function handleLogout() {
+    Logout();
+    navigate("/login");
   }
   return (
-    <div ref={navRef} className='navbar'>
-     <div className="navbar-left">
-        <img src={Logo} alt="" className='navbarimage' />
+    <div ref={navRef} className="navbar">
+      <div className="navbar-left">
+        <img src={Logo} alt="" className="navbarimage" />
         <ul>
           <li onClick={homenavigate}>Home</li>
           <li>TV Shows</li>
@@ -41,19 +56,19 @@ export const Navbar = () => {
           <li onClick={navbarchange}>My List</li>
           <li>Browse by Language</li>
         </ul>
-     </div>
-     <div className="navbar-right">
-      <img src={search_icon} className='icons' />
-      <p>Children</p>
-      <img src={bell_icon} className='icons'  />
-      <div className="navbar-profile">
-        <img src={prfile_img} className='profile'  />
-        <img src={caret_icon}/>
-        <div className="dropdown">
-           <p onClick={handleLogout}>Sign Out of Netflix</p>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} className="icons" />
+        <p>Children</p>
+        <img src={bell_icon} className="icons" />
+        <div className="navbar-profile">
+          <img src={prfile_img} className="profile" />
+          <img src={caret_icon} />
+          <div className="dropdown">
+            <p onClick={handleLogout}>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
-     </div>
     </div>
-  )
-}
+  );
+};
